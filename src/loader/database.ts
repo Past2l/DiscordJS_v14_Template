7face3a3660b2928ca4af7bc8719577f853eb515
Dohyeon Yun <to@dohyun.it>
2023-10-14 22:04:53 +0900

    add: `environment.ts` enableDB

--- src/loader/database.ts
+++ src/loader/database.ts
@@ -1,20 +1,22 @@
 import { createConnection } from 'typeorm';
+import { enableDB } from './environment';
 
 export default async () => {
-  await createConnection({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || ''),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [`${__dirname}/../**/*.entity.{ts,js}`],
-    migrations: [],
-    subscribers: [],
-    charset: 'utf8mb4',
-  })
-    .then(() => console.log('Database Connected'))
-    .catch((error) => console.error(error));
+  if (enableDB)
+    await createConnection({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || ''),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: false,
+      entities: [`${__dirname}/../**/*.entity.{ts,js}`],
+      migrations: [],
+      subscribers: [],
+      charset: 'utf8mb4',
+    })
+      .then(() => console.log('Database Connected'))
+      .catch((error) => console.error(error));
 };
--- src/loader/environment.ts
+++ src/loader/environment.ts
@@ -1,5 +1,6 @@
-export const ENVIRONMENT_NAMES = [
-  'BOT_TOKEN',
+export const ENVIRONMENT_NAMES = ['BOT_TOKEN'];
+
+export const DB_ENVIRONMENT_NAMES = [
   'DB_HOST',
   'DB_PORT',
   'DB_USERNAME',
@@ -7,7 +8,11 @@ export const ENVIRONMENT_NAMES = [
   'DB_NAME',
 ];
 
+export let enableDB = true;
+
 export default async () => {
   for (const name of ENVIRONMENT_NAMES)
     if (!process.env[name]) throw new Error(`Environment '${name}' is empty`);
+  for (const name of DB_ENVIRONMENT_NAMES)
+    if (!process.env[name] && enableDB) enableDB = false;
 };
